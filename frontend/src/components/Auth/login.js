9de797dbb7589e105e63b35c35999429e12b8040
hiwa2719 <hiahmadyan@gmail.com>
2022-08-20 22:22:24 +0430

    adding graphql to project

--- frontend/src/components/Auth/login.js
+++ frontend/src/components/Auth/login.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Lock from "@material-ui/icons/Lock";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/client";
 
+const LOGIN_QUERY = gql`
+    mutation($username: String!, $password: String!){
+        tokenAuth(username: $username, password: $password){
+            token
+        }
+    }
+`
 
 const Login = ({setLoginScreen}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [getToken, token] = useMutation(LOGIN_QUERY, {
+        onCompleted: (data)=> {
+            localStorage.setItem('authToken', data.tokenAuth.token)
+        }
+    })
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        getToken({
+            variables: {
+                username: username,
+                password: password
+            }
+        })
+        console.log(getToken)
+        console.log(token)
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <span className="text-light rounded-circle bg-primary p-2">
-                <Lock />
+                <Lock/>
             </span>
             <h3 className="mt-3">Login as Existing User</h3>
             <label htmlFor="username" className="d-block text-start">Username</label>
-            <input type="text" id="username" className="form-control my-3" placeholder="Username"/>
+            <input type="text" id="username" className="form-control my-3" placeholder="Username"
+                   onChange={(e) => setUsername(e.target.value)}/>
             <label htmlFor="password" className="d-block text-start">password</label>
-            <input type="password" id="Password" className="form-control my-3" placeholder="Password"/>
+            <input type="password" id="Password" className="form-control my-3" placeholder="Password"
+                   onChange={(e) => setPassword(e.target.value)}/>
             <input type="submit" className="btn btn-primary my-3 w-100" value="Login"/>
             <button className="btn btn-outline-warning w-100" onClick={() => setLoginScreen(false)}>
                 New user? Register here
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+ uri: 'http://127.0.0.1:8000/graphql/',
+ cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
