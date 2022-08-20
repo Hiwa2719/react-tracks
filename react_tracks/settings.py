3f519fe1d452401a9fe0dd47262f3d137f2f5a80
hiwa <hiwa@DESKTOP-TTKD9F6.localdomain>
2022-08-20 18:47:39 +0430

    adding react app

--- react_tracks/settings.py
+++ react_tracks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'graphene_django',
 
     'tracks',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,5 @@ GRAPHENE = {
         "graphql_jwt.middleware.JSONWebTokenMiddleware",
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
